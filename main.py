b154eadd0dc8cb6d973de8948d5148a43a38ef9d
Beau Rogers <brogers1812@gmail.com>
2021-07-26 17:51:06 -1000

    update

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+from typing import get_type_hints
 import streamlit as st
 
-
-human_year = int(input("How old is your dog in years?"))
-human_month = int(input("How old is your dog in months?"))
-
+st.title("Dog Age Convertor")
+html_temp="A basic dog age convertor<br> July 26, 2021 <br> By. Sophia & Beau Rogers "
+st.markdown(html_temp,unsafe_allow_html = True)
+human_year = st.number_input('Enter your dog age in human years',value=5,min_value=0,max_value=25,step=1)
+human_month = st.number_input('Enter your dog age in human months',value=5,min_value=0,max_value=12,step=1)
 if human_year == 0:
-    dog_age = float(human_month*84/12/12)
-    dog_age_months = dog_age % 1
-    dog_age_months = int(dog_age_months*12)
-    print("Your dog is {} years and {} months old.".format(int(dog_age),dog_age_months))
+	dog_age = float(human_month*84/12/12)
+	dog_age_months = dog_age % 1
+	dog_age_months = int(dog_age_months*12)
+	st.text("Your dog is {} years and {} months old.".format(int(dog_age),dog_age_months))
 else:
-    dog_age_month = float(human_month*84/12/12)
-    dog_age_months = dog_age_month % 1
-    dog_age_months = int(dog_age_months*12)
-    dog_age = human_year*7 + dog_age_month
-    print("Your dog is {} years and {} months old.".format(int(dog_age),dog_age_months))
+	dog_age_month = float(human_month*84/12/12)
+	dog_age_months = dog_age_month % 1
+	dog_age_months = int(dog_age_months*12)
+	dog_age = human_year*7 + dog_age_month
+	st.text("Your dog is {} years and {} months old.".format(int(dog_age),dog_age_months))
